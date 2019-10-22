370d55c48157644daf7fd50fabe1d788f093a30e
Teresa <teresa.lenz@desy.de>
2019-10-22 17:32:12 +0200

    some aesthetic changes + take only second column of y_pred + y_true

--- losses.py
+++ losses.py
@@ -73,7 +73,7 @@ def asimovLossInvert(expectedSignal,expectedBkgd,systematic):
 # ------------------------------------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------------------------------------
 
-def asimovLossInvertWithReg(expectedSignal,expectedBkgd,systematic):
+def asimovLossInvertWithReg(expectedSignal, expectedBkgd, systematic):
 
     # systematic = K.print_tensor(systematic, 'systematic=')
 
@@ -82,9 +82,9 @@ def asimovLossInvertWithReg(expectedSignal,expectedBkgd,systematic):
         # Contrain y_pred from below and above by epsilon
         y_pred = tf.clip_by_value(y_pred, K.epsilon(), 1 - K.epsilon())
 
-        # Take only first column of y_pred and y_true -> study this further
-        # y_pred = y_pred[:,1]
-        # y_true = y_true[:,1]
+        # Take only second column of y_pred and y_true -> study this further which column to take
+        y_pred = y_pred[:,1]
+        y_true = y_true[:,1]
 
 
         ############################   Printing #####################################
@@ -102,9 +102,6 @@ def asimovLossInvertWithReg(expectedSignal,expectedBkgd,systematic):
         # Calculate an overall signal and background weight
         sigWeight = expectedSignal/K.sum(y_true)
         bkgWeight = expectedBkgd/K.sum(1-y_true)
-
-        sigWeight = 1.
-        bkgWeight = 1.
 
         s = sigWeight*K.sum(y_pred*y_true)
         b = bkgWeight*K.sum(y_pred*(1-y_true))
@@ -125,28 +122,28 @@ def asimovLossInvertWithReg(expectedSignal,expectedBkgd,systematic):
 # ------------------------------------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------------------------------------
 
-def asimovLossInvertWithRegWeighted(systematic):
+def asimovLossInvertWithRegWeighted(expectedSignal=1, expectedBkgd=1, systematic=0.0):
 
-    def sigLoss(y_true,y_pred,weights):
+    def sigLoss(y_true, y_pred, weights):
 
         # with additional argument : https://stackoverflow.com/questions/48082655/custom-weighted-loss-function-in-keras-for-weighing-each-element
         # without additional argument : https://datascience.stackexchange.com/questions/25029/custom-loss-function-with-additional-parameter-in-keras
 
-        #y_true , weights = decode_weights(y_true)
-        # y_true  = y_true/weights
-        # y_true  = (y_true+1)/2.
+        # Take only signal column from y_pred and y_true
+        y_pred = y_pred[:,1]
+        y_true = y_true[:,1]
 
-        #weights = K.print_tensor(weights, 'weights=')
-
-        #s = K.sum(y_pred * y_true * weights     ) #* weights)
-        #b = K.sum(y_pred * (1-y_true) * weights ) #* weights)
+        # Calculate s and b where s = sum_signal_events(y_pred) and b = sum_bkg_events(y_pred)
+        #s = K.sum(y_pred * y_true * weights     )
+        #b = K.sum(y_pred * (1-y_true) * weights )
         s = K.sum(y_pred * y_true  ) #* weights)
         b = K.sum(y_pred * (1-y_true) ) #* weights)
-        sigB=systematic*b
-        sigma_reg = 1.8410548  # this is  68% CL from the Neyman construction for N=0 -> thus the lowest statistical uncertainty that can be achieved (see : https://twiki.cern.ch/twiki/bin/viewauth/CMS/PoissonErrorBars)
-        sigB = K.sqrt(sigB*sigB + sigma_reg*sigma_reg)
 
-        return 1./(2*((s+b)*K.log((s+b)*(b+sigB*sigB)/(b*b+(s+b)*sigB*sigB))-b*b*K.log(1+sigB*sigB*s/(b*(b+sigB*sigB)))/(sigB*sigB)))
+        sigma_b   = systematic*b
+        sigma_reg = 1.8410548  # this is  68% CL from the Neyman construction for N=0 -> thus the lowest statistical uncertainty that can be achieved (see : https://twiki.cern.ch/twiki/bin/viewauth/CMS/PoissonErrorBars)
+        sigma_b   = K.sqrt( sigma_b*sigma_b + sigma_reg*sigma_reg )
+
+        return 1./(2*((s+b)*K.log((s+b)*(b+sigma_b*sigma_b)/(b*b+(s+b)*sigma_b*sigma_b))-b*b*K.log(1+sigma_b*sigma_b*s/(b*(b+sigma_b*sigma_b)))/(sigma_b*sigma_b)))
 
     return sigLoss
 
