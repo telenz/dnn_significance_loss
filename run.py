d33411b2f8b6124a456054d7f097a97aed75815e
Teresa <teresa.lenz@desy.de>
2019-10-25 08:38:21 +0200

    some aesthetics

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ import ConfigParser
 import keras
 #import tensorflow as tf
 #from keras import backend as K
-from keras.models import model_from_json
+#from keras.models import model_from_json
 #from sklearn import preprocessing
 #from sklearn.model_selection import train_test_split
 #from sklearn.metrics import roc_auc_score
@@ -22,6 +22,8 @@ np.random.seed(1234) # for reproducibility
 from tensorflow import set_random_seed
 set_random_seed(3)
 
+
+#----------------------------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------------------------
 ##### Higgs data #####
 ######################
@@ -39,7 +41,8 @@ X_train = fcn.add_weight_corrected_by_lumi(X_train, Y_train, config)
 X_test  = fcn.add_weight_corrected_by_lumi(X_test, Y_test, config)
 #----------------------------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------------------------
-# Define the network architecture and relevant settings and train the network
+#### Define network and train ####
+##################################
 #----------------------------------------------------------------------------------------------------
 reload(loss)
 reload(arch)
@@ -49,28 +52,19 @@ reload(fcn)
 input_weights = None
 model, input_weights = arch.model_for_weights(num_inputs = len(features), num_outputs = 2)
 #model = arch.susy_2(num_inputs = len(features), num_outputs = 2)
-
 #----------------------------------------------------------------------------------------------------
 # Define callbacks
 cb = fcn.define_callbacks(config)
 cb_list = [cb]
-
 #----------------------------------------------------------------------------------------------------
 # Add optimizer options
 keras.optimizers.Adam(lr=float(config.get('KERAS','learning_rate')))
-
 #----------------------------------------------------------------------------------------------------
 # Get the right loss function (from the config)
-reload(loss)
-reload(arch)
-reload(fcn)
-s_exp = float(config.get('PARAMETERS','s_exp'))
-b_exp = float(config.get('PARAMETERS','b_exp'))
-
 if config.get('KERAS','loss') != 'binary_crossentropy':
     loss_function = getattr( loss, config.get('KERAS','loss') )
     #loss_from_config = loss_function(s_exp,b_exp,float(config.get('KERAS','systematic')))
-    loss_from_config = loss_function(s_exp, b_exp, float(config.get('KERAS','systematic')))
+    loss_from_config = loss_function(float(config.get('PARAMETERS','s_exp')), float(config.get('PARAMETERS','b_exp')), float(config.get('KERAS','systematic')))
     print loss_from_config
 else:
     loss_from_config = 'binary_crossentropy'
@@ -82,7 +76,6 @@ if input_weights is not None:
 else:
     loss_=loss_from_config
     print loss_
-#loss_=loss_from_config
 print loss_
 
 #Y_train  = fcn.encode_weights(Y_train['signal'],X_train['Weight_corrected_by_lumi'])
@@ -92,7 +85,6 @@ model.compile(loss=loss_,
               metrics=[config.get('KERAS','metrics')])
 
 d=model.summary()
-
 #----------------------------------------------------------------------------------------------------
 # Training or Reading model
 if config.get('KERAS','train')=="True":
@@ -100,12 +92,13 @@ if config.get('KERAS','train')=="True":
     history =  fcn.train_model(model, X_train, Y_train, features, 
                                cb_list, config, sample_weights = '')
 #----------------------------------------------------------------------------------------------------
-
-# Prediction for test data set
-#-----------------------------
+#----------------------------------------------------------------------------------------------------
+#### Predict and visualize results ####
+#######################################
+# Prediction
+#-----------
 reload(fcn)
 df_pred = fcn.make_prediction(model, X_test, Y_test, features, config)
-
 #----------------------------------------------------------------------------------------------------
 # Visualization
 #--------------
@@ -118,21 +111,20 @@ vis.plot_val_train_loss(history, plot_log = False)
 vis.plot_prediction(df_pred)
 # Get significance estimates
 vis.plot_significances(df_pred, "Weight_corrected_by_lumi")
-
-
 #----------------------------------------------------------------------------------------------------
-# Save the output of this test in a folder
-#----------------------------------------
-folder_name = time.strftime("%Y_%m_%d_%H_%M_%S")
-if not os.path.exists(folder_name):
-    os.makedirs(folder_name)
-os.system('cp keras.cfg ' + folder_name +'/keras.cfg')
-os.system('cp plots/classification.png ' + folder_name +'/.')
-os.system('cp plots/significance_estimates.png ' + folder_name +'/.')
-if config.get('KERAS','train')=="True":
-    os.system('cp plots/loss.png ' + folder_name +'/.')    
-else:
-    os.system('cp -r models/ ' + folder_name +'/.')    
-
-
 #----------------------------------------------------------------------------------------------------
+# =============================================================================
+# # Save the output of this test in a folder
+# #----------------------------------------
+# folder_name = time.strftime("%Y_%m_%d_%H_%M_%S")
+# if not os.path.exists(folder_name):
+#     os.makedirs(folder_name)
+# os.system('cp keras.cfg ' + folder_name +'/keras.cfg')
+# os.system('cp plots/classification.png ' + folder_name +'/.')
+# os.system('cp plots/significance_estimates.png ' + folder_name +'/.')
+# if config.get('KERAS','train')=="True":
+#     os.system('cp plots/loss.png ' + folder_name +'/.')
+# else:
+#     os.system('cp -r models/ ' + folder_name +'/.')
+# #----------------------------------------------------------------------------------------------------
+# =============================================================================
