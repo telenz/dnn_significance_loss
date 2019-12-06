24b5c9757900443adb4b4d0820d0db0db93669d1
Teresa <teresa.lenz@desy.de>
2019-12-06 13:45:00 +0100

    update draw_sigmoid.py

--- utils/draw_sigmoid.py
+++ utils/draw_sigmoid.py
@@ -4,8 +4,8 @@ import pylab as plt
 import numpy as np
 
 #sigmoid = lambda x: 1 / (1 + np.exp(-x))
-def sigmoid(x):
-    return (1 / (1 + np.exp(-x)))
+def sigmoid(alpha, x):
+    return (1 / (1 + np.exp(-alpha*x)))
 
 mySamples = []
 mySigmoid = []
@@ -15,29 +15,25 @@ mySigmoid = []
 # with requested number of elements. Example 10 elements or 100 elements.
 # 
 x = plt.linspace(-0.5,0.5,1000)
-y = plt.linspace(-0.5,0.5,100000)
 
 # prepare the plot, associate the color r(ed) or b(lue) and the label 
-plt.plot(x, sigmoid(x), 'r', label='10 bins')
-plt.plot(y, sigmoid(y), 'b', label='100 bins')
+plt.plot(x, sigmoid(10, x), 'g', label='alpha = 10')
+plt.plot(x, sigmoid(20, x), 'b', label='alpha = 20')
+plt.plot(x, sigmoid(50, x), 'r', label='alpha = 50')
+plt.plot(x, sigmoid(100, x), 'c', label='alpha = 100')
+plt.plot(x, sigmoid(200, x), 'm', label='alpha = 200')
 
 # Draw the grid line in background.
 plt.grid()
 
 # Title & Subtitle
 plt.title('Sigmoid Function')
-plt.suptitle('Sigmoid')
 
 # place the legen boc in bottom right of the graph
 plt.legend(loc='lower right')
 
 # write the Sigmoid formula
 plt.text(4, 0.8, r'$\sigma(x)=\frac{1}{1+e^{-x}}$', fontsize=15)
-
-# #resize the X and Y axes
-# plt.gca().xaxis.set_major_locator(plt.MultipleLocator(1))
-# plt.gca().yaxis.set_major_locator(plt.MultipleLocator(0.1))
- 
 
 # plt.plot(x)
 plt.xlabel('X Axis')
