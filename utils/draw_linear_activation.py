a2e9a11c8efdcf4339d94c095832f9ac48fefca4
Teresa <teresa.lenz@desy.de>
2019-12-09 18:19:51 +0100

    first commit

---
+++ utils/draw_linear_activation.py
@@ -0,0 +1,43 @@
+#import section
+from matplotlib import pylab
+import pylab as plt
+import numpy as np
+
+#sigmoid = lambda x: 1 / (1 + np.exp(-x))
+def f_three_range(s_1, s_2, s_3, x_1, x_2, y_pred):
+
+    f_1 = np.maximum( s_1*y_pred , 0 ) - np.maximum( s_1*(y_pred-x_1) , 0)
+    f_2 = np.maximum( s_2*(y_pred-x_1), 0) - np.maximum( s_2*(y_pred-x_2), 0)
+    f_3 = np.maximum( s_3*(y_pred-x_2), 0) - np.maximum( s_3*(y_pred-1), 0)
+
+    return f_1 + f_2 + f_3
+
+# generate an Array with value ???
+# linespace generate an array from start and stop value
+# with requested number of elements. Example 10 elements or 100 elements.
+# 
+x = np.linspace(-0.1,1.1,1000)
+
+# prepare the plot, associate the color r(ed) or b(lue) and the label 
+plt.plot(x, f_three_range(0.1, 1.0, 0.1, 0.4, 0.6, x), 'g', label='alpha = 10')
+
+# Draw the grid line in background.
+plt.grid()
+
+# Title & Subtitle
+plt.title('Sigmoid Function')
+
+# place the legen boc in bottom right of the graph
+plt.legend(loc='lower right')
+
+# write the Sigmoid formula
+plt.text(4, 0.8, r'$\sigma(x)=\frac{1}{1+e^{-x}}$', fontsize=15)
+
+# plt.plot(x)
+plt.xlabel('X Axis')
+plt.ylabel('Y Axis')
+
+#plt.yscale('log')
+
+# create the graph
+plt.show()
